56d3f3a8baf3cc915ce0e5870f2bc81d08468a99
LeoMedeirosss <lmf@cesar.school>
2025-05-10 17:08:04 -0300

    tailwind1.0 - first commit

---
+++ Learning Tailwind/learning-tailwid/src/app/page.tsx
@@ -0,0 +1,7 @@
+ export default function Home() {
+  return (
+    <div className="p-1.5 h-screen bg-slate-700">
+      <h1 className="text-gray-950 font-bold text-3xl">Learning Tailwind CSS</h1>
+    </div>
+  );
+}
