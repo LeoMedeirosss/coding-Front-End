518dfcc4217eba237cba48b167b8828cf5148f9b
LeoMedeirosss <lmf@cesar.school>
2025-05-12 21:13:45 -0300

    Tailwind:Seletores e Estados

--- Learning Tailwind/learning-tailwid/src/app/page.tsx
+++ Learning Tailwind/learning-tailwid/src/app/page.tsx
@@ -1,7 +1,12 @@
  export default function Home() {
   return (
     <div className="p-1.5 h-screen bg-slate-700">
-      <h1 className="text-gray-950 font-bold text-3xl">Learning Tailwind CSS</h1>
+      <h1 className="text-gray-950 font-bold text-3xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 before:flex">
+        Learning Tailwind CSS
+      </h1>
+      <button className="bg-sky-500 px-4 py-2 rounded-lg text-white font-bold hover:bg-sky-600 mt-6 ml-6 hover:scale-105 transition-all">
+        botão
+      </button>
     </div>
   );
 }
