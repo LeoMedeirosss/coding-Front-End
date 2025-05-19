70c7f7cdf9693e8498b7fab16fc38d098c8724d9
LeoMedeirosss <lmf@cesar.school>
2025-05-19 20:37:37 -0300

    Tailwind sidebar 1.0

--- Learning Tailwind/learning-tailwid/src/app/layout.tsx
+++ Learning Tailwind/learning-tailwid/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Sidebar } from "@/components/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <div className="grid min-h-screen grid-cols-[235px_1fr]">
-          <aside className="border-r border-r-zinc-200 px-5 py-8">sidebar</aside>
+          <Sidebar/>
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
       </body>
---
+++ Learning Tailwind/learning-tailwid/src/components/sidebar/index.tsx
@@ -0,0 +1,15 @@
+export interface SidebarProps{}
+
+export function Sidebar(props:SidebarProps) {
+    return(
+        <aside className="border-r border-r-zinc-200 px-5 py-8">
+            <strong className="flex items-center text-xl font-semibold text-zinc-900">
+                <span>Untitled UI</span>
+            </strong>
+
+            <div className=" ">
+            </div>
+        </aside>
+    )
+}
+//faltando 7:10 para acabar o vídeo
