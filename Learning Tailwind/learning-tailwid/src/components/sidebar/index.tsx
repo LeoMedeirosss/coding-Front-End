f4cbb49bd321c46a77a926ea8515cba777437457
LeoMedeirosss <lmf@cesar.school>
2025-06-16 18:22:17 -0300

    Tailwind - sidebar 3.0

--- Learning Tailwind/learning-tailwid/src/components/sidebar/index.tsx
+++ Learning Tailwind/learning-tailwid/src/components/sidebar/index.tsx
@@ -1,11 +1,10 @@
-import { Search } from "lucide-react";
-import { MainNavitation } from "./MainNavegation";
+import { Home, BarChart, CheckSquare, Flag, Search, SquareStack, Users } from "lucide-react";
+import { NavItem } from "./NavItem";
+import { UsedSpaceWidget } from "./UsedSpaceWidget";
 
-export interface SidebarProps{}
-
-export function Sidebar(props:SidebarProps) {
+export function Sidebar() {
     return(
-        <aside className="border-r border-r-zinc-200 px-5 py-8 space-y-5">
+        <aside className="flex flex-col gap-6 border-r border-r-zinc-200 px-5 py-8">
             <strong className="flex items-center text-xl font-semibold text-zinc-900">
                 <span>Untitled UI</span>
             </strong>
@@ -14,7 +13,16 @@ export function Sidebar(props:SidebarProps) {
                 <input type="text" placeholder="Search" className="flex-1 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"/>
             </div>
 
-            <MainNavitation/>
+        <nav className="space-y-0.5 ">
+            <NavItem title="Home" icon={Home} />
+            <NavItem title="Dashboard" icon={BarChart} />
+            <NavItem title="Projects" icon={SquareStack} />
+            <NavItem title="Tasks" icon={CheckSquare} />
+            <NavItem title="Reporting" icon={Flag} />
+            <NavItem title="Users" icon={Users} />
+        </nav>
+
+        <UsedSpaceWidget/>
         </aside>
     )
 }
