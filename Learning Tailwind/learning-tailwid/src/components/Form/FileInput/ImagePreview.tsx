e23500edda8582c663de1ba5c86ef68a56a23357
LeoMedeirosss <lmf@cesar.school>
2025-07-01 17:59:17 -0300

    Tailwind - context/image preview

--- Learning Tailwind/learning-tailwid/src/components/Form/FileInput/ImagePreview.tsx
+++ Learning Tailwind/learning-tailwid/src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,29 @@
+"use client"
+
 import { User } from "lucide-react";
+import { useFileInput } from "./Root";
+import { useMemo } from "react";
 
 export function ImagePreview() {
-    return(
-        <div className="bg-violet-50 h-16 w-16 rounded-full flex items-center justify-center">
-            <User className="w-8 h-8 text-violet-500"/>
-        </div>
-    )
+    const { files } = useFileInput()
+
+    const previewURL = useMemo(() => {
+        if (files.length === 0) {
+            return null
+        }
+
+        return URL.createObjectURL(files[0])
+    }, [files])
+
+    if(previewURL === null) {
+        return(
+            <div className="bg-violet-50 h-16 w-16 rounded-full flex items-center justify-center">
+                <User className="w-8 h-8 text-violet-500"/>
+            </div>
+        )
+    } else {
+        return(
+            <img src={previewURL} alt="" className="h-16 w-16 rounded-full object-cover"/>
+        )
+    }
 }
