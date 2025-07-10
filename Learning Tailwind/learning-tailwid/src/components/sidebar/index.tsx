6964b854b03d4806f1aef6630bed7d3de6344469
LeoMedeirosss <lmf@cesar.school>
2025-07-10 18:34:33 -0300

    Tailwind - sidebar responsivity

--- Learning Tailwind/learning-tailwid/src/components/sidebar/index.tsx
+++ Learning Tailwind/learning-tailwid/src/components/sidebar/index.tsx
@@ -1,12 +1,12 @@
 import { Home, BarChart, CheckSquare, Flag, Search, SquareStack, Users } from "lucide-react";
 import { NavItem } from "./NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
-import { Profile } from "./profile";
+import { Profile } from "./Profile";
 import * as Input from "../Input";
 
 export function Sidebar() {
     return(
-        <aside className="flex flex-col gap-6 border-r border-r-zinc-200 px-5 py-8">
+        <aside className="flex flex-col gap-6 border-b border-zinc-200 px-5 py-8 left-0 top-0 bottom-0 p-4 right-0 fixed bg-white z-20 lg:right-auto lg:w-75 lg:border-r lg:px-5 lg-py-7 lg:relative">
             <strong className="flex items-center text-xl font-semibold text-zinc-900">
                 <span>Untitled UI</span>
             </strong>
